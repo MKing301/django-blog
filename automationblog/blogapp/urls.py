6b8707af7db7ddbc4fc71e21d657cc8aa75d593e
MKING301 <33675411+MKing301@users.noreply.github.com>
2024-12-08 14:17:46 -0500

    Add url pattern for deleting post

--- automationblog/blogapp/urls.py
+++ automationblog/blogapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView
+from .views import (
+    HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView
+)
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
@@ -8,4 +10,5 @@ urlpatterns = [
     ),
     path('add_post/', AddPostView.as_view(), name='add_post'),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
+    path('article/<int:pk>/delete', DeletePostView.as_view(), name='delete_post'),
 ]
